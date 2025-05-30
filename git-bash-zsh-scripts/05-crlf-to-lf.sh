012d80b7b645b18f7cd432f577d32a7844468fc0
subllings <102749651+subllings@users.noreply.github.com>
2025-05-30 12:33:48 +0200

    Typo

--- git-bash-zsh-scripts/05-crlf-to-lf.sh
+++ git-bash-zsh-scripts/05-crlf-to-lf.sh
@@ -4,6 +4,6 @@
 # ensuring Unix-style line endings (LF only).
 sed -i 's/\r$//' myscript.sh
 
-# It grants execute permission so you can run it directly using 
-# Usage using Zsh: ./myscript.sh.
+# It grants execute permission so you can run it directly  
+# using Zsh: ./myscript.sh.
 chmod +x myscript.sh
