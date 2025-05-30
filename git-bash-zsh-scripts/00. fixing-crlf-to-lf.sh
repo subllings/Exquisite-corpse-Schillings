9aef57b4ce31bc0ed9930473f86d914b942b5afd
subllings <102749651+subllings@users.noreply.github.com>
2025-05-30 13:18:43 +0200

    Typo script

--- git-bash-zsh-scripts/00. fixing-crlf-to-lf.sh
+++ git-bash-zsh-scripts/00. fixing-crlf-to-lf.sh
@@ -1,4 +1,4 @@
-4# Removes Windows-style carriage returns (CR) at the end of lines.
+# Removes Windows-style carriage returns (CR) at the end of lines.
 # It replaces \r characters (carriage return) with nothing (//), 
 # ensuring Unix-style line endings (LF only).
 sed -i 's/\r$//' fix-all-scripts.sh
